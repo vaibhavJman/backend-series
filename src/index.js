34348c50af2c7bd4bb27fb10fe1344b1fbfb60ed
Vaibhav Agarwal <vaibhav.a@jmangroup.com>
2024-10-23 12:16:30 +0530

    Readme file added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import dotenv from "dotenv";
 import connectDB from "./db/db.js";
+import express from "express";
+
+const app = express();
+
+const port = process.env.PORT || 8000;
 
 dotenv.config();
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`App is listening on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connection failed ", err);
+  });
