d75dd975ecc74af25b7c548431468c4b63f10dba
Vaibhav Agarwal <vaibhav.a@jmangroup.com>
2024-11-13 18:37:22 +0530

    Added toggleSubscription()

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -6,8 +6,54 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const toggleSubscription = asyncHandler(async (req, res) => {
-  const { channelId } = req.params;
   // TODO: toggle subscription
+  const { channelId } = req.params;
+  if (!isValidObjectId(channelId)) {
+    throw new ApiError(400, "Invalid Channel ID");
+  }
+
+  const channel = await User.findById(channelId);
+  if (!channel) {
+    throw new ApiError(404, "Channel not found!");
+  }
+
+  const existingSubscribed = await Subscription.findOne({
+    subscriber: req.user?._id,
+    channel: channelId,
+  });
+
+  let updateSubscription;
+  if (!existingSubscribed) {
+    updateSubscription = await Subscription.updateOne(
+      {
+        subscriber: req.user?._id,
+        channel: channelId,
+      },
+      {
+        $set: {
+          subscriber: req.user?._id,
+          channel: channelId,
+        },
+      },
+      {
+        upsert: true,
+      } // Upsert: true creates a document if it doesn’t exist
+    );
+  } else {
+    updateSubscription = await Subscription.deleteOne({
+      _id: existingSubscribed._id,
+    });
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        updateSubscription,
+        "Video subscribed Toggled Successfully"
+      )
+    );
 });
 
 // controller to return subscriber list of a channel
