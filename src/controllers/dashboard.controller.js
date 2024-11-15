39639985c2d5eb516b3583351e76e8bb214643eb
Vaibhav Agarwal <vaibhav.a@jmangroup.com>
2024-11-15 18:19:21 +0530

    Started Dashboard Controller

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,13 +1,77 @@
 import mongoose from "mongoose";
+import { User } from "../models/user.model.js";
 import { Video } from "../models/video.model.js";
 import { Subscription } from "../models/subscription.model.js";
-// import { Like } from "../models/like.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const getChannelStats = asyncHandler(async (req, res) => {
   // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+
+  const subscriberCount = await Subscription.aggregate([
+    //Stage-1
+    {
+      $match: {
+        channel: new mongoose.Types.ObjectId(req.user?._id),
+      },
+    },
+    {
+      $count: "Subscriber Count",
+    },
+  ]);
+
+  const videoCount = await Video.aggregate([
+    //Stage-1
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(req.user?._id),
+      },
+    },
+    //Stage -2
+    {
+      $count: "Video Count",
+    },
+  ]);
+
+  const likesCount = await Video.aggregate([
+    //Stage-1
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(req.user?._id),
+      },
+    },
+    //Stage -2
+    {
+      $lookup: {
+        from: "likevideos",
+        localField: "_id",
+        foreignField: "video",
+        as: "likes",
+        pipeline: [
+          //Stage -3
+          {
+            $count: "likes",
+          },
+        ],
+      },
+    },
+
+    //Stage-4
+    {
+      $addFields: {
+        likes: {
+          $first: "$likes",
+        },
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, likesCount, "Channel Stats fetched Successfully!")
+    );
 });
 
 const getChannelVideos = asyncHandler(async (req, res) => {
